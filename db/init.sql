db24f83c28f049942f135be285b36be871b84b88
Darius Balanica <darius.balanica@stud.acs.upb.ro>
2020-03-04 09:52:55 +0200

    Initialized the database and created corresponding tables for the store

--- db/init.sql
+++ db/init.sql
@@ -1,38 +1,23 @@
-CREATE DATABASE flights;
-use flights;
+CREATE DATABASE store;
+USE store;
 
-/*
-Tabela cu zboruri, in care sunt retinute toate detaliile referitoare la zboruri,
-conform enuntului 
- */
-CREATE TABLE IF NOT EXISTS Flights (
-	ID INT,
-	Source VARCHAR(100),
-	Destination VARCHAR(100),
-	Hour INT,
-	Day INT,
-	Duration INT,
-	Seats INT
+CREATE TABLE IF NOT EXISTS Products (
+        ProductID INT,
+        Name VARCHAR(100),
+        Category VARCHAR(100),
+        Price FLOAT,
+        Stock INT
 );
 
-/*
-Tabela utila pentru operatiile de rezervare a zborurilor, in care sunt retinute 
-capacitatea zborului si numarul de rezervari pentru un anumit zbor intr-un anumit 
-moment de timp
- */
-CREATE TABLE IF NOT EXISTS Booking_Capacity(
-	Flight_ID INT,
-	Flight_Capacity INT,
-	Booked_Seats INT
+CREATE TABLE IF NOT EXISTS Users (
+        UserID INT,
+        Username VARCHAR(100),
+        Password VARCHAR(100)
 );
 
-/*
-Tabela pentru evidenta rezervarilor, in care sunt retinute ID-ul rezervarii si 
-un string ce reprezinta Id-urile zborurilor pentru care s-a efectuat rezervarea 
-respectiva separate prin spatiu
- */
-CREATE TABLE IF NOT EXISTS Reservations (
-	Reservation_ID VARCHAR(100),
-	Flights_IDs VARCHAR(100)
+CREATE TABLE IF NOT EXISTS Cart (
+        UserID INT,
+        ProductID INT,
+        Quantity INT
 );
 
